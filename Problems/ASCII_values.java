90aebaa47bfdfb1c033e1ec6d010796716098a33
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2024-11-21 20:41:37 +0530

    problems tackle

---
+++ Problems/ASCII_values.java
@@ -0,0 +1,18 @@
+package problems;
+
+public class ASCII_values {
+
+	public static void main(String[] args)
+	{
+		System.err.println("\n ASCII value of a to z");
+	  char	max='z';
+	 // char max='Z'
+		//for(char ch='a';ch<=max;ch++)
+		for(char ch='A'; ch<=max;ch++)
+		{
+			int man=ch;
+			System.out.println(ch +" = " +man);
+		}
+	}
+
+}
---
+++ Problems/even_without_mod.java
@@ -0,0 +1,21 @@
+package problems;
+import java.util.*;
+public class even_without_mod {
+
+	public static void main(String[] args)
+	{
+		Scanner t = new Scanner(System.in);
+		System.err.println(" enter any nuber of your choice \n");
+		int n = t.nextInt();
+		
+		if( n/2*2==n)
+			System.out.println("\n given number is even" );
+		else
+			System.out.println("\n  your number is odd ");
+			
+				
+		
+
+	}
+
+}
