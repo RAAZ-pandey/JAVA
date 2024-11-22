ac8f2a962728a51a588ba15c0881d9ae0cea038d
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2024-11-22 20:43:01 +0530

    static problems

---
+++ Static ques/Arrcompare.java
@@ -0,0 +1,20 @@
+package static_ques;
+
+public class Arrcompare {
+
+	public static void main(String[] args)
+	{
+		int arr[] = {1,2,3};
+		int arr1[]= {1,2,3};
+		
+		
+		if(arr == arr1)
+		{
+			System.out.println("pagla");
+		}
+		else
+			System.out.println("pagli");
+
+	}
+
+}
---
+++ Static ques/Output_check.java
@@ -0,0 +1,12 @@
+package static_ques;
+
+import static java.lang.System.*;
+public class Output_check {
+
+	public static void main(String[] args)
+	{
+		out.println("BE POSITIVE");
+
+	}
+
+}
