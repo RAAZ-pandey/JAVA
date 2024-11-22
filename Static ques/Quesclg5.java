b0da7a5fe73e97c86790bf9a64f0c1b166a10b13
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2024-11-22 20:47:02 +0530

    update

---
+++ Static ques/Quesclg5.java
@@ -0,0 +1,17 @@
+package static_ques;
+
+public class Quesclg5 {
+
+	public static void main(String[] args)
+	{
+		for(int i = 0;i<15;i++)
+		{
+			if(i%3==0)  // 0,3,6,9,12 won't print anything
+				continue;
+			System.out.println("Hello");
+           
+		}
+
+	}
+
+}
