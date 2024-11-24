b3efcf052bd84cf8acced526f770a3ff8342719e
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2024-11-24 22:07:58 +0530

    imp question

---
+++ Exception Questions/Question7.java
@@ -0,0 +1,18 @@
+package exception;
+
+public class Question7 {
+
+	public static void main(String[] args) 
+	{
+		try
+		{
+			return ;
+		}
+		finally
+		{
+			System.out.println("finally");
+		}
+
+	}
+
+}
