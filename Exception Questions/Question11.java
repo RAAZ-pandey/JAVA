fda84d324ff736f2dc0e7a6767074fda776c4811
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2024-11-25 20:40:55 +0530

    imp question

---
+++ Exception Questions/Question11.java
@@ -0,0 +1,27 @@
+package exception;
+
+public class Question11 {
+
+	public static void throwit()
+	{
+		System.out.println("throwit");
+		throw new RuntimeException();
+	}
+	public static void main(String[] args) 
+	{
+		try
+		{
+			System.out.println("hello");
+			throwit();
+		}catch(Exception re)
+		{
+			System.out.println("caught");
+		}
+		finally
+		{
+			System.out.println("finally");
+		}
+         System.out.println("after");
+	}
+
+}
