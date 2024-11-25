ed91b1c1a61cac3df3ff770cc8c44b0c400d0677
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2024-11-25 20:46:09 +0530

    trick problem

---
+++ Exception Questions/Question17.java
@@ -0,0 +1,18 @@
+package exception;
+
+public class Question17 {
+
+	public static void main(String[] args) 
+	{
+		// try
+		//{
+			//throw //10 ;
+		//}
+          //catch(int e)
+	//	{
+        	//  System.out.println("got the Excepyion" +e);
+		//}
+		System.out.println( "raju bhai");
+	}
+
+}
