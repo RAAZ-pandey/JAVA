ef550e5aea3c4161abcbb7f1b84eb91aedd50d3a
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2024-11-25 20:39:27 +0530

    imp question

---
+++ Exception Questions/Question10.java
@@ -0,0 +1,31 @@
+package exception;
+
+public class Question10 {
+
+	public static void main(String[] args)
+	{
+		try
+		{
+			badmethod();
+			System.out.println("A");
+		}
+		catch(RuntimeException ex)
+		{
+			System.out.println("B");
+		}
+		catch(Exception ex1)
+		{
+			System.out.println("C");
+		}
+       finally
+       {
+    	   System.out.println("D");
+       }
+		System.out.println("E");
+	}
+	public static void badmethod()
+	{
+		throw new RuntimeException();
+	}
+
+}
