0d0bf30869a870e5e32d08b8e16131c3e9b0963a
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2024-11-27 21:24:05 +0530

    problems of string

---
+++ String problems/doubleequal.java
@@ -0,0 +1,19 @@
+package String_ques;
+
+public class doubleequal {
+
+	public static void main(String[] args)
+	{
+	  String s = "india" ;
+	  String a= "india" ;
+	  String f= " INDIA" ;
+	  String v= new  String("india");
+	  
+	  System.out.println(s==a);
+	  System.out.println(a==f);
+	  System.out.println(f==v);
+	  System.out.println(s==v);
+
+	}
+
+}
