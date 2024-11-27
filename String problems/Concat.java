77838c2487ea19891384e788bb505c5f9d149270
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2024-11-27 21:21:48 +0530

    problem of string

---
+++ String problems/Concat.java
@@ -0,0 +1,17 @@
+package String_ques;
+
+public class Concat {
+
+	public static void main(String[] args)
+	{
+		
+		String s1 = "GAME" ;
+		String s2 = "OVER" ;
+		String str = "RUNTIME" ;
+		
+		System.out.println(s1.concat(s2));
+        System.out.println(str.indexOf('T'));
+        System.out.println(str.substring(2, 5));
+	}
+
+}
