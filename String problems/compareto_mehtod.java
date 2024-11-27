ac106fd848df384fe6d649a7575228a9398f97c5
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2024-11-27 21:20:59 +0530

    problem of string

---
+++ String problems/compareto_mehtod.java
@@ -0,0 +1,14 @@
+package String_ques;
+
+public class compareto_mehtod {
+
+	public static void main(String[] args)
+	{
+		String s="radhe" ;
+		String a="kanha" ;
+		String b="KANHA" ;
+       System.out.println(s.compareTo(a));
+       System.out.println(a.compareToIgnoreCase(b));
+	}
+
+}
