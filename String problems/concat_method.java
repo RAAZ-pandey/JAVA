6b41144fde87e3b1463a53027d82fed561cda3f3
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2024-11-27 21:23:08 +0530

    problems of string

---
+++ String problems/concat_method.java
@@ -0,0 +1,15 @@
+package String_ques;
+
+public class concat_method {
+	
+	public static void main(String[] args)
+	{
+		String a= " raj is" ;
+		String b =" also known as godfather" ;
+		String p=a.concat( b);
+		System.out.println(a);
+		System.out.println( b);
+		System.out.println(p);
+	}
+
+}
