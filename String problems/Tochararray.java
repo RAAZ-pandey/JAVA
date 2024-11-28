93c6e59c63f2f482e6a3bd5158a2663ed6ecf630
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2024-11-28 22:10:32 +0530

    problems of string

---
+++ String problems/Tochararray.java
@@ -0,0 +1,18 @@
+package String_ques;
+import java.util.*;
+public class Tochararray {
+
+	public static void main(String[] args)
+	{
+		//String a="panparag";
+		//char m[]=a.toCharArray();
+		//System.out.println(m[5]);
+		
+		Scanner t=new Scanner(System.in);
+		System.out.println("\n write your fav word");
+		String a=t.next();
+		char c[]=a.toCharArray();
+		System.out.println(c[3]);
+	}
+
+}
