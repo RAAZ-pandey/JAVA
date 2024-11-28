6f5f93543c63a69625b9352cf5616d036dbd2798
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2024-11-28 22:08:07 +0530

    problem of string

---
+++ String problems/length_chat_method.java
@@ -0,0 +1,14 @@
+package String_ques;
+
+public class length_chat_method {
+
+	public static void main(String[] args)
+	{
+		String s= "radheshyam is a  good boy" ;
+        System.out.println(s.length());
+        System.out.println(s.charAt(5));   // always rem index starts from 0
+        System.out.println(s.charAt(18));
+        System.out.println(s.charAt(0));     /*like this*/
+	}
+
+}
