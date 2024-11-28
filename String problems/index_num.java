562983fe3c1eb88934af4bf5be347b2aff1c6afb
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2024-11-28 22:06:14 +0530

    problems of string

---
+++ String problems/index_num.java
@@ -0,0 +1,13 @@
+package String_ques;
+
+public class index_num {
+
+	public static void main(String[] args) 
+	{
+		String a1= new String("siyaramchandra ki jay");
+		 int x=a1.indexOf("i");
+         System.out.println(a1.indexOf("s"));
+         System.out.println(a1.lastIndexOf("a"));
+	}
+
+}
