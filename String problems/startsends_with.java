6483f4445a1d1b08a5f03d744e888ad69bab777d
Rajkumar pandey <Raj.11165@stu.upes.ac.in>
2024-11-28 22:09:30 +0530

    problems of string

---
+++ String problems/startsends_with.java
@@ -0,0 +1,14 @@
+package String_ques;
+
+public class startsends_with {
+
+	public static void main(String[] args)
+	{
+		String t="indicamemrya" ;
+		System.out.println(t.startsWith("i"));
+		System.out.println(t.endsWith("i"));
+		System.out.println(t.endsWith("a"));
+
+	}
+
+}
